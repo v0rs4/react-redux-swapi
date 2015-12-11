b7d1ee4731e8c1be81e3b402d288a189b1da1d06
vladislavpauk <vladislavpauk@gmail.com>
2015-12-11 12:05:58 +0200

    Require bower deps in client.dev.js

--- src/client.dev.js
+++ src/client.dev.js
@@ -4,6 +4,10 @@ import createStore from './redux/createStore.dev';
 import { Provider } from 'react-redux';
 import { ReduxRouter } from 'redux-router';
 
+require('bootstrap/dist/css/bootstrap.min.css');
+require('normalize-css/normalize.css');
+require('animate.css/animate.min.css');
+
 const store = createStore();
 
 render(
