1a714e85f35ec6cd5422c9beb6ddd192464e2a8a
vladislavpauk <vladislavpauk@gmail.com>
2015-12-11 13:00:03 +0200

    Update nav list

--- src/layouts/Application/Application.js
+++ src/layouts/Application/Application.js
@@ -8,7 +8,7 @@ export default React.createClass({
       <div className="container fadeIn animated">
         <h1>Star Wars API & React Redux</h1>
         <nav>
-          <ul>
+          <ul className="nav nav-pills">
             <li><Link to='/'>People</Link></li>
             <li><Link to='/films'>Films</Link></li>
           </ul>
