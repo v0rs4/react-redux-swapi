74f630c2259c394725c56baa40135b630147d60c
vladislavpauk <vladislavpauk@gmail.com>
2015-12-11 12:05:33 +0200

    Make webpack require css\fonts properly

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -9,12 +9,18 @@ module.exports = {
   },
   module: {
     loaders: [
-      { test: /\.js$/, exclude: /node_modules/, loader: 'react-hot!babel' }
+      { test: /\.js$/, exclude: /node_modules/, loader: 'react-hot!babel' },
+      { test: /\.css$/, loader: 'style!css' },
+      { test: /\.woff(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/font-woff" },
+      { test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/font-woff" },
+      { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/octet-stream" },
+      { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file" },
+      { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=image/svg+xml" },
     ]
   },
   resolve: {
     extensions: ['', '.js'],
-    modulesDirectories: ['node_modules', 'src']
+    modulesDirectories: ['node_modules', 'bower_components', 'src']
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
