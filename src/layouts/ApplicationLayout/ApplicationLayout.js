01bbc31cd386bde9ec89bbe91166ac4ffaf3d1cb
vladislavpauk <vladislavpauk@gmail.com>
2015-12-23 22:25:00 +0200

    Add fonts and navbar

--- src/layouts/ApplicationLayout/ApplicationLayout.js
+++ src/layouts/ApplicationLayout/ApplicationLayout.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router';
 import { connect } from 'react-redux';
 import { pushState } from 'redux-router';
 import './application-layout.scss';
@@ -28,29 +29,57 @@ export class ApplicationLayout extends React.Component {
     return (
       <div>
         <div className="left-sidebar">
+          <div className="logo">
+            <i className="swi-rebel"></i>
+            <span>SWAPI</span>
+            <span>React / Redux</span>
+          </div>
           <ul>
             <li>
-              <a href="#">People</a>
+              <Link to="/">
+                <i className="swi-people"></i>
+                <span>People</span>
+              </Link>
             </li>
             <li>
-              <a href="#">Films</a>
+              <Link to="/films">
+                <i className="swi-video"></i>
+                Films
+              </Link>
             </li>
             <li>
-              <a href="#">Planets</a>
+              <a href="#">
+                <i className="swi-globe"></i>
+                Planets
+              </a>
             </li>
             <li>
-              <a href="#">Starships</a>
+              <a href="#">
+                <i className="swi-tie-fighter"></i>
+                Starships
+              </a>
             </li>
             <li>
-              <a href="#">Species</a>
+              <a href="#">
+                <i className="swi-people"></i>
+                Species
+              </a>
             </li>
             <li>
-              <a href="#">Vehicles</a>
+              <a href="#">
+                <i className="swi-vehicle"></i>
+                Vehicles
+              </a>
             </li>
           </ul>
         </div>
-        <div className="main-content">
-          {this.props.children}
+        <div className="main-area">
+          <div className="navbar">
+            <a href="#" className="hide-btn">Hide</a>
+          </div>
+          <div className="main-content">
+            {this.props.children}
+          </div>
         </div>
       </div>
     );
